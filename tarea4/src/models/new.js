d0c7933663a07c13740c05bbd4b4ad975b0478f8
arturocch <arturocch@outlook.com>
2023-03-13 21:13:54 -0600

    simplificacion de modelo tarea4

--- tarea4/src/models/new.js
+++ tarea4/src/models/new.js
@@ -1,11 +1,11 @@
 const axios = require('axios')
 
 const noticia = {
-    buscar: async(keyword) => {
+    buscar: (keyword) => {
 
         const url = `https://newsapi.org/v2/everything?q=${keyword}&apiKey=${process.env.NEWS_APIKEY}`
         console.log(url)
-        const news = await axios.get(url)
+        const news =  axios.get(url)
 
         return news
 
